22ccd3842ed5fa9094c82c02616bd098af1fa0b3
s-babineau <sean.babineau@bounteous.com>
2025-02-14 21:11:01 -0400

    Update product-details.js

--- blocks/product-details/product-details.js
+++ blocks/product-details/product-details.js
@@ -19,6 +19,7 @@ import ProductQuantity from '@dropins/storefront-pdp/containers/ProductQuantity.
 import ProductDescription from '@dropins/storefront-pdp/containers/ProductDescription.js';
 import ProductAttributes from '@dropins/storefront-pdp/containers/ProductAttributes.js';
 import ProductGallery from '@dropins/storefront-pdp/containers/ProductGallery.js';
+import ProductDetails from '@dropins/storefront-pdp/containers/ProductDetails.js';
 import trackViewedProduct from '../../scripts/api/productTracking.js';
 
 // Libs
@@ -253,6 +254,213 @@ export default async function decorate(block) {
 
     // Attributes
     pdpRendered.render(ProductAttributes, {})($attributes),
+
+    pdpRendered.render(ProductDetails, {
+      sku: product.sku,
+      hideSku: true,
+      hideURLParams: true,
+      hideQuantity: true,
+      hideShortDescription: true,
+      hideDescription: true,
+      hideAttributes: true,
+      carousel: {
+        controls: {
+          desktop: 'thumbnailsRow',
+          mobile: 'thumbnailsRow',
+        },
+        arrowsOnMainImage: true,
+        loopable: false,
+        peak: {
+          mobile: false,
+          desktop: false,
+        },
+        gap: 'small',
+        thumbnailParams: false,
+        imageParams: false,
+        thumbnailsLoadingMode: 'lazy',
+      },
+      slots: {
+        SpecialPrice: (ctx) => {
+          // Ratings
+          const ratings = ctx.data.attributes.find((attribute) => attribute.id === 'hvt_29')?.value || null;
+          const reviewsCount = ctx.data.attributes.find((attribute) => attribute.id === 'hvt_51')?.value || null;
+          ctx.appendSibling(createProductRatingStructure(product.name, ratings, reviewsCount));
+        },
+        Actions: (ctx) => {
+          // handle cart protection plan
+          const mainContent = document.querySelector('#main-content');
+          const cartProtectionPopup = document.createElement('div');
+          mainContent.appendChild(cartProtectionPopup);
+          cartProtectionPopup.id = 'cart-protection-plan';
+          window.addEventListener('click', (event) => {
+            if (event.target.id === 'cart-protection-plan') {
+              cartProtectionPopup.style.bottom = '-100%';
+            }
+          });
+          // Add to Cart Button
+          ctx.appendButton((next, state) => {
+            // eslint-disable-next-line no-shadow
+            const addingToCart = async (next, state) => {
+              try {
+                state.set('adding', true);
+
+                if (!next.valid) {
+                  console.error('Invalid product', next.values);
+                  return;
+                }
+
+                const variantSku = document.querySelector('.pdp-product')?.getAttribute('data-variant-sku');
+                const defaultSku = ctx.data.attributes.find((attr) => attr.id === 'hvt_30')?.value || null;
+                next.values.sku = variantSku ?? defaultSku;
+
+                const addToCartResp = await addProductsToCart([{ ...next.values }]);
+                store.setCartId(addToCartResp.id);
+
+                // update the browser persistance cartId
+                let browserCartData = localStorage.getItem('M2_VENIA_BROWSER_PERSISTENCE__cartId');
+                if (browserCartData) {
+                  browserCartData = JSON.parse(browserCartData);
+                  browserCartData.value = `"${addToCartResp.id}"`;
+                  browserCartData.timeStored = Date.now();
+                  localStorage.setItem('M2_VENIA_BROWSER_PERSISTENCE__cartId', JSON.stringify(browserCartData));
+                }
+                const cartQuantity = document.getElementById('cartQuantity');
+                cartQuantity.innerHTML = `Cart (${addToCartResp.totalQuantity})`;
+
+                if (addToCartResp.totalQuantity) {
+                  cartApi.toggleCart();
+                }
+              } catch (error) {
+                console.error('Error adding product to cart', error);
+              } finally {
+                state.set('adding', false);
+              }
+            };
+
+            const handleProtectionPlan = (parentCategory) => {
+              cartProtectionPopup.innerHTML = mobileProtectionPlan(parentCategory);
+              cartProtectionPopup.style.bottom = '0';
+              cartProtectionPopup.style.display = 'flex';
+
+              const closePopup = document.querySelector('.close-popup-pdp');
+              closePopup.addEventListener('click', () => {
+                cartProtectionPopup.style.bottom = '-100%';
+              });
+
+              const addProtection = async () => {
+                cartProtectionPopup.style.bottom = '-100%';
+                const cartCount = store.getCart();
+                if (cartCount.total_quantity === 0) {
+                  sessionStorage.clear();
+                  itemSequenceNumber = 1;
+                  sessionStorage.setItem('itemSequenceNumber', itemSequenceNumber);
+                }
+                const items = {
+                  id: itemSequenceNumber,
+                  product: {
+                    sku: next.values.sku,
+                  },
+                  warranty_type: 'GOLD_PLAN',
+                };
+                try {
+                  await addingToCart(next, state);
+                  await addWarrantyToCartItem(items);
+                } catch (error) {
+                  console.error(`Error adding warranty for item with sequenceNumber: ${itemSequenceNumber}`, error);
+                } finally {
+                  itemSequenceNumber++;
+                  sessionStorage.setItem('itemSequenceNumber', itemSequenceNumber);
+                }
+              };
+
+              const noThanks = async () => {
+                cartProtectionPopup.style.bottom = '-100%';
+                await addingToCart(next, state);
+              };
+
+              document.querySelector('#add-protection').addEventListener('click', addProtection);
+              document.querySelector('#no-thanks').addEventListener('click', noThanks);
+            };
+
+            return {
+              text: state.get('adding')
+                ? next.dictionary.Custom.AddingToCart?.label
+                : next.dictionary.PDP.Product.AddToCart?.label,
+              icon: 'Cart',
+              variant: 'primary',
+              disabled: state.get('adding') || !next.data.inStock,
+              onClick: async () => {
+                const breadcrumbLinks = document.querySelectorAll('.breadcrumb-links a');
+                const parentCategory = breadcrumbLinks.length > 1 ? breadcrumbLinks[1].textContent.trim() : null;
+                const protectionCategories = ['Living Room', 'Bedroom', 'Dining Room'];
+                const { mobile } = detectDevice();
+
+                if (parentCategory && protectionCategories.includes(parentCategory) && mobile) {
+                  handleProtectionPlan(parentCategory);
+                } else {
+                  await addingToCart(next, state);
+                }
+              },
+            };
+          });
+
+          // Delivery estimate
+          const deliveryEstimateContainer = document.createElement('div');
+          ctx.appendSibling(deliveryEstimateContainer);
+
+          Promise.all([
+            loadCSS(`${window.hlx.codeBasePath}/blocks/delivery-estimate/delivery-estimate.css`),
+            import('../delivery-estimate/delivery-estimate.js'),
+          ])
+            .then(([, { default: decorateDeliveryEstimate }]) => {
+              const loadEstimateDelivery = (newCtx) => {
+                const variantSku =
+                  newCtx.data.variantSku ??
+                  newCtx.data.attributes.find((attribute) => attribute?.id === 'hvt_30')?.value ??
+                  newCtx.data.sku;
+                const availableInStores = newCtx.data.attributes.find((attribute) => attribute?.id === 'hvt_56')?.value;
+
+                deliveryEstimateContainer.dataset.availableInStores = availableInStores;
+                deliveryEstimateContainer.dataset.variantSku = variantSku;
+
+                // Decorate delivery estimate block.
+                decorateDeliveryEstimate(deliveryEstimateContainer);
+              };
+              loadEstimateDelivery(ctx);
+              ctx.onChange(loadEstimateDelivery);
+            })
+            .catch((error) => console.error(error));
+        },
+        Options: (ctx) => {
+          ctx.onChange(async (event) => {
+            const { data } = event;
+
+            // Emit a pdp/data-from-slot event since the pdp/data event is broken.
+            events.emit('pdp/data-from-slot', {
+              ...product,
+              ...data,
+            });
+
+            if (data.externalParentId) {
+              const variantsku = data.variantSku ? data.variantSku : data.images[0].label;
+              // Set or Update Product Variant SKU
+              setVariantSKU(variantsku);
+
+              // To avoid on initial page load and execute only on variant change
+              if (!pageLoadFlag) {
+                // Load zoom viewers
+                import('./gallery.js').then(({ loadZoomViewers }) => {
+                  loadZoomViewers();
+                });
+              }
+            }
+          });
+        },
+      },
+      // eslint-disable-next-line no-console
+      onAddToCart: (values) => console.log('Added to cart', values),
+      useACDL: true,
+    })(block),
   ]);
 
   if (product) {
